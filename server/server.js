0e04b53ec798f4e66381638b0f3608e09136ad08
Dominick Simone <domsimone123@gmail.com>
2021-10-24 12:48:59 -0400

    Adding Apollo Server and GraphQL to server.js and file structure

--- server/server.js
+++ server/server.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const routes = require('./routes');
 const path = require('path');
+const { ApolloServer } = require('apollo-server-express');
+
 const sequelize = require('./config/connection');
+
+const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
+  
+server.applyMiddleware({ app });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,7 +23,9 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
