43f82390a12f0fc73a44cf7ca70b7d8e2fae74fd
Dominick Simone <domsimone123@gmail.com>
2021-11-01 18:15:31 -0400

    Adding Post Component

---
+++ client/src/components/Post.js
@@ -0,0 +1,17 @@
+const Post = () => {
+    return (
+        <div className="singlePostOuterDiv">
+            <div className="singlePostHeaderDiv">
+                <h4 className="marginOne">First_Name @Username</h4>
+            </div>
+            <div className="singlePostTextDiv">
+                <p>Post_text Post Text Post_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post Text Post_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post Text</p>
+            </div>
+            <div className="singlePostFooterDiv">
+                <h4 className="marginOne">Likes</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Post
