d59ceda8d0629ad8beb7b17e9b39117116efa596
Dominick Simone <domsimone123@gmail.com>
2021-11-01 18:15:07 -0400

    fixing seeds not hashing passwords

--- server/seeds/index.js
+++ server/seeds/index.js
@@ -5,7 +5,7 @@ const {userSeeds, followsSeeds, postSeeds, likesSeeds} = require("./seeds")
 
 const seedDatabase = async () => {
     await sequelize.sync({ force: true });
-    const users = await User.bulkCreate(userSeeds)
+    const users = await User.bulkCreate(userSeeds, {individualHooks: true})
     const posts = await Post.bulkCreate(postSeeds)
     const likes = await Likes.bulkCreate(likesSeeds)
     const follows = await Follows.bulkCreate(followsSeeds)
