de78c3a38d5131a3a48358101730ded8ca6ddeed
Dominick Simone <domsimone123@gmail.com>
2021-11-02 18:59:54 -0400

    adding apollo and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,35 @@
-import Post from "./components/Post"
-import CreatePost from "./components/CreatePost";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from "./pages/Profile"
+import Header from './components/Header';
 import './App.css';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div>
-      <h1>Hello There</h1>
-      <CreatePost />
-      <Post />
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <Route exact path="/user/:id">
+              <Profile />
+            </Route>
+          </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
