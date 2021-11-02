22f1f17cd23195786ca757c74e7a7e9660ebf928
Dominick Simone <domsimone123@gmail.com>
2021-11-02 18:59:28 -0400

    fixing queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,13 +1,14 @@
+import {gql} from "@apollo/client"
 export const QUERY_USER = gql`
   query users {
     user {
-        id: ID!
-        username: String!
-        first_name: String!
-        createdAt: String!
-        posts: [Post]
-        followers: [User]
-        following: [User]
+        id
+        username
+        first_name
+        createdAt
+        posts
+        followers
+        following
     }
   }
 `;
