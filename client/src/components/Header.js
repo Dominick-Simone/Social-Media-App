ea4cbc357527b5b6cff23ec1157eed70da42637a
Dominick Simone <domsimone123@gmail.com>
2021-11-02 19:00:05 -0400

    updated navbar routing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
     return (
         <header>
             <ul>
-                <a className="linkStyles" href="#Home"><li className="navigationStyles">Home</li></a>
-                <a className="linkStyles" href="#Profile"><li className="navigationStyles">Login</li></a>
-                <a className="linkStyles" href="#Login"><li className="navigationStyles"> Logout</li></a>
+                <a className="linkStyles" href="/"><li className="navigationStyles">Home</li></a>
+                <a className="linkStyles" href="/login"><li className="navigationStyles">Login</li></a>
+                <a className="linkStyles" href="/logout"><li className="navigationStyles"> Logout</li></a>
             </ul>
         </header>
     )
