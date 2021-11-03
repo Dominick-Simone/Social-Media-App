c04712804c2161ebb1fb6aeb7993681f5ee09e73
Dominick Simone <domsimone123@gmail.com>
2021-11-03 15:18:28 -0400

    adding link to users profile in each post

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,8 +1,9 @@
+import {Link} from "react-router-dom"
 const Post = ({username, firstName, postText}) => {
     return (
         <div className="singlePostOuterDiv">
             <div className="singlePostHeaderDiv">
-                <h4 className="marginOne">{firstName} @{username}</h4>
+                <h4 className="marginOne">{firstName} @<Link to={`/${username}`}>{username}</Link></h4>
             </div>
             <div className="singlePostTextDiv">
                 <p>{postText}</p>
