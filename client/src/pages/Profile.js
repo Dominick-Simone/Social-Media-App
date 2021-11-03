04aac9b1111e7cce9f9b7bc0c74f086120b2793b
Dominick Simone <domsimone123@gmail.com>
2021-11-02 21:20:26 -0400

    creating post page that generates all of a users posts

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,26 @@
 import React from 'react'
-
+import Post from "../components/Post"
+import { useQuery, useMutation } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+import { QUERY_USER, QUERY_USER_BY_USERNAME } from '../utils/queries';
 const Profile = () => {
+    let { username } = useParams();
+
+    const { loading, data } = useQuery(QUERY_USER_BY_USERNAME, {
+        variables: { username: username },
+      });
+      const user = data?.user || [];
+      console.log(user)
+    if(loading) {
+        return <h1>Loading</h1>;
+    }
     return (
         <>
-            
+
+            <h3>{`${user.first_name} @${user.username}`}</h3>
+            {user.posts.map((post) => {
+                return <Post username={user.username} firstName={user.first_name} postText={post.post_text}/>
+            })}
         </>
     )
 }
