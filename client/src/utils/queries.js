fec9447f0f40ce254e81e59c6d69c5d44eae3980
Dominick Simone <domsimone123@gmail.com>
2021-11-02 21:20:38 -0400

    creating front end gql queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,14 +1,41 @@
-import {gql} from "@apollo/client"
+import { gql } from "@apollo/client"
+
 export const QUERY_USER = gql`
-  query users {
-    user {
+query users {
+    users {
         id
         username
         first_name
         createdAt
-        posts
-        followers
-        following
+        posts {
+          post_text
+        }
+        followers {
+          username
+        }
+        following {
+          username
+        }
+    }
+  }
+`;
+
+export const QUERY_USER_BY_USERNAME = gql`
+query userByUsername($username: String!) {
+    user(username: $username) {
+        id
+        username
+        first_name
+        createdAt
+        posts {
+          post_text
+        }
+        followers {
+          username
+        }
+        following {
+          username
+        }
     }
   }
 `;
