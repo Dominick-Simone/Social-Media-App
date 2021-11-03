9d08f62b2582197f23a88cac223085088f08c074
Dominick Simone <domsimone123@gmail.com>
2021-11-02 21:20:09 -0400

    updating post template to generate based on given inputs

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,11 +1,11 @@
-const Post = () => {
+const Post = ({username, firstName, postText}) => {
     return (
         <div className="singlePostOuterDiv">
             <div className="singlePostHeaderDiv">
-                <h4 className="marginOne">First_Name @Username</h4>
+                <h4 className="marginOne">{firstName} @{username}</h4>
             </div>
             <div className="singlePostTextDiv">
-                <p>Post_text Post Text Post_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post Text Post_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post TextPost_text Post Text</p>
+                <p>{postText}</p>
             </div>
             <div className="singlePostFooterDiv">
                 <h4 className="marginOne">Likes</h4>
