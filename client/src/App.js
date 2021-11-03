a8a2c274b49e18427b67ae414aaee415160eeb5b
Dominick Simone <domsimone123@gmail.com>
2021-11-02 21:19:05 -0400

    fixing params

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
             <Route exact path="/login">
               <Login />
             </Route>
-            <Route exact path="/user/:id">
+            <Route exact path="/profile/:username">
               <Profile />
             </Route>
           </Switch>
