bb11d22fb85f5692cf4b90c3a2c33a2aa4177239
Dominick Simone <domsimone123@gmail.com>
2021-11-03 15:18:38 -0400

    generating posts to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import Post from "../components/Post"
+import CreatePost from "../components/CreatePost"
+import { useQuery, useMutation } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+import { QUERY_POSTS, QUERY_USER, QUERY_USER_BY_USERNAME } from '../utils/queries';
 const Home = () => {
+    let { username } = useParams();
+
+    const { loading, data } = useQuery(QUERY_POSTS);
+      const posts = data?.posts || [];
+      console.log(posts)
+    if(loading) {
+        return <h1>Loading</h1>;
+    }
     return (
         <>
-            <Post />
+            {/* if logged in */}
+            <CreatePost />
+            {posts.map((post) => {
+                return <Post username={post.user.username} firstName={post.user.first_name} postText={post.post_text}/>
+            })}
         </>
     )
 }
