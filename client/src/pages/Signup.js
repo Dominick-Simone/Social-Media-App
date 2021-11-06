dd31277245077f2a3085132bb81ed285aebac97b
Dominick Simone <domsimone123@gmail.com>
2021-11-06 17:25:04 -0400

    added signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import { Link } from "react-router-dom"
+import SignupForm from '../components/SignupForm'
 const Signup = () => {
     return (
         <>
-            
+            <SignupForm />
+            <p className="signUpText">Already have an account? <Link to="/login">Login Here</Link></p>
         </>
     )
 }
