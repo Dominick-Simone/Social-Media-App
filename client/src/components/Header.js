a22c994b6d5cec36679b2085f30838dcc87483ee
Dominick Simone <domsimone123@gmail.com>
2021-11-06 17:21:14 -0400

    added conditional rendering if logged in

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,24 @@
 import React from 'react'
-
+import Auth from "../utils/auth";
 const Header = () => {
     return (
         <header>
             <ul>
-                <a className="linkStyles" href="/"><li className="navigationStyles">Home</li></a>
-                <a className="linkStyles" href="/login"><li className="navigationStyles">Login</li></a>
-                <a className="linkStyles" href="/profile"><li className="navigationStyles"> Logout</li></a>
+                {Auth.loggedIn() ? (
+                <>
+                    <a className="linkStyles" href="/"><li className="navigationStyles">Home</li></a>
+                    <a className="linkStyles" href="/" onClick={() => Auth.logout()}><li className="navigationStyles"> Logout</li></a>
+                </>
+                ) : (
+                <>
+                    <a className="linkStyles" href="/"><li className="navigationStyles">Home</li></a>
+                    <a className="linkStyles" href="/login"><li className="navigationStyles">Login</li></a>
+                </>
+
+                )
+
+                }
+                
             </ul>
         </header>
     )
