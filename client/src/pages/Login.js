bfd6890fd5368650206374bd687706d08524600c
Dominick Simone <domsimone123@gmail.com>
2021-11-06 17:23:27 -0400

    added login page

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import {Link} from "react-router-dom"
+import LoginForm from "../components/LoginForm"
 const Login = () => {
     return (
         <>
-            
+            <LoginForm />
+            <p className="loginText">Don't have an account? <Link to="/signup">Sign Up Here</Link></p>
         </>
     )
 }
