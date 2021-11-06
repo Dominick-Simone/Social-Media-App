34097cfea76970c444edf27680b2e4c8e6742eee
Dominick Simone <domsimone123@gmail.com>
2021-11-06 17:19:55 -0400

    updating resolvers with jwt

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,6 @@
 const {User, Likes, Post, Follows} = require("../models/index.js");
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -19,6 +21,11 @@ const resolvers = {
           }
         ] });
         return user;
+      },
+      getLikes: async (parent, { post_id }) => {
+        const likes = await Likes.findAndCountAll({ where: { post_id: post_id }})
+        
+        return likes.count;
       },
       posts: async () => {
 
@@ -48,7 +55,7 @@ const resolvers = {
         }
         )
         console.log(likes)
-        return likes
+        return likes;
       },
       follows: async () => {
         const follows = await Follows.findAll()
@@ -58,16 +65,35 @@ const resolvers = {
     },
     Mutation: {
       createUser: async (parent, {username, first_name, last_name, email, password}) => {
-        return await User.create({username, first_name, last_name, email, password})
+        const user = await User.create({username, first_name, last_name, email, password})
+        const token = signToken(user)
+        return {token, user}
       },
-      createPost: async (parent, {user_id, post_text}) => {
-        return await Post.create({user_id, post_text})
+      createPost: async (parent, {post_text}, context) => {
+        return await Post.create({user_id: context.user.id, post_text})
       },
-      addLike: async (parent, {user_liked_by, post_id}) => {
-        return await Likes.create({user_liked_by, post_id})
+      addLike: async (parent, {post_id}, context) => {
+        return await Likes.create({user_liked_by: context.user.id, post_id})
       },
-      addFollow: async (parent, {followed, follower}) => {
-        return await Follows.create({followed_id: followed, follower_id: follower})
+      addFollow: async (parent, {followed}, context) => {
+        return await Follows.create({followed_id: followed, follower_id: context.user.id})
+      },
+      login: async (parent, { username, password }) => {
+        const user = await User.findOne({where: { username: username }});
+        console.log(user)
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const correctPw = await user.isCorrectPassword(password);
+  
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+  
+        const token = signToken(user);
+  
+        return { token, user };
       }
     }
 };
