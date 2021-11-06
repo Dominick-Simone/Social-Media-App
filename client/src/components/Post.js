bd0886bb89867107da28eaca19c802d39a69ef5f
Dominick Simone <domsimone123@gmail.com>
2021-11-06 17:22:34 -0400

    added likes being loaded from database

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,5 +1,16 @@
 import {Link} from "react-router-dom"
-const Post = ({username, firstName, postText}) => {
+import { useQuery } from '@apollo/client';
+import { GET_LIKES } from '../utils/queries';
+
+const Post = ({username, firstName, postText, postId}) => {
+
+    const { loading, data } = useQuery(GET_LIKES, {
+        variables: { post_id: parseInt(postId) },
+      });
+      const likes = data?.getLikes || 0;
+      if(loading) {
+          return <h1></h1>;
+        }
     return (
         <div className="singlePostOuterDiv">
             <div className="singlePostHeaderDiv">
@@ -9,7 +20,7 @@ const Post = ({username, firstName, postText}) => {
                 <p>{postText}</p>
             </div>
             <div className="singlePostFooterDiv">
-                <h4 className="marginOne">Likes</h4>
+                <h4 className="marginOne" datapostid={postId}>Likes {likes}</h4>
             </div>
         </div>
     )
