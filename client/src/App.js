066425ba24ccce6c54e5e4ed729d2860cfdb2fd8
Dominick Simone <domsimone123@gmail.com>
2021-11-06 17:21:01 -0400

    implementing jwt

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Profile from "./pages/Profile"
 import Header from './components/Header';
+import Signup from './pages/Signup';
 import './App.css';
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: '/graphql',
+});
+const client = new ApolloClient({
+  link: httpLink,
   cache: new InMemoryCache(),
+});
+
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('id_token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
 });
 
 function App() {
@@ -23,6 +40,9 @@ function App() {
             </Route>
             <Route exact path="/login">
               <Login />
+            </Route>
+            <Route exact path="/signup">
+              <Signup />
             </Route>
             <Route exact path="/:username">
               <Profile />
