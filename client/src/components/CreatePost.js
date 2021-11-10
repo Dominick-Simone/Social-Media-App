a1645467f96be035b951715f675b00ecda41b4c5
Dominick Simone <domsimone123@gmail.com>
2021-11-10 13:46:17 -0500

    finishing createPost

--- client/src/components/CreatePost.js
+++ client/src/components/CreatePost.js
@@ -1,20 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 
+import { useMutation } from '@apollo/client';
+
+import { CREATE_POST } from '../utils/mutations';
+import Auth from '../utils/auth';
 const CreatePost = () => {
+    const [formState, setFormState] = useState({
+        post_text: '',
+        user_id: Auth.getProfile().data.id
+      });
+    const [createPost, { error, data }] = useMutation(CREATE_POST);
+
+    const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+    try {
+      const { data } = await createPost({
+        variables: { ...formState },
+      });
+      console.log(data)
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      post_text: '',
+    });
+  };
     return (
-        <div className="createPostOuterDiv">
+        <form className="createPostOuterDiv" onSubmit={handleFormSubmit}>
             <div className="createPostHeaderDiv">
                 <h3 className="marginOne">Create Post</h3>
             </div>
             <div className="createPostTextAreaDiv">
-                <textarea className="createPostTextArea" placeholder="Enter a message..." rows="5"></textarea>
+                <textarea className="createPostTextArea" placeholder="Enter a message..." rows="5" value={formState.post_text} name="post_text" onChange={handleChange}></textarea>
             </div>
             <div className="createPostFooterDiv">
                 <a>
-                <h4 className="createPostBtn">Post</h4>
+                <button disabled={!formState.post_text} type="submit" className="postButton" value="Submit">Post</button>
                 </a>
             </div>
-        </div>
+        </form>
     )
 }
 
