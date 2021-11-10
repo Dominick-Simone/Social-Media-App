ee421a5cac5553e8384339639c1860736b0cbb9d
Dominick Simone <domsimone123@gmail.com>
2021-11-10 13:46:55 -0500

    updating header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -7,11 +7,13 @@ const Header = () => {
                 {Auth.loggedIn() ? (
                 <>
                     <a className="linkStyles" href="/"><li className="navigationStyles">Home</li></a>
+                    <a className="linkStyles" href={`/${Auth.getProfile().data.username}`}><li className="navigationStyles">Dashboard</li></a>
+                    <a className="linkStyles" href="/discover"><li className="navigationStyles">Discover</li></a>
                     <a className="linkStyles" href="/" onClick={() => Auth.logout()}><li className="navigationStyles"> Logout</li></a>
                 </>
                 ) : (
                 <>
-                    <a className="linkStyles" href="/"><li className="navigationStyles">Home</li></a>
+                    <a className="linkStyles" href="/discover"><li className="navigationStyles">Discover</li></a>
                     <a className="linkStyles" href="/login"><li className="navigationStyles">Login</li></a>
                 </>
 
