9c922674322d12eb8061944550afc2f76bea5537
Dominick Simone <domsimone123@gmail.com>
2021-11-10 13:49:26 -0500

    updating resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -7,20 +7,36 @@ const resolvers = {
       users: async () => {
         const users = await User.findAll(
           {
-          include: [Post, 'followers', 'following']
+          include: 
+          [
+            {model: Post, include: [User, Likes]},
+            "followers", 
+            "following"
+          ]
         }
         )
         console.log(users)
-        return users
+        return users;
       },
       user: async (parent, { username }) => {
         const params = username ? { username } : {};
-        const user = await User.findOne({ where: { username: params.username }, include: [
-          {
-            model: Post
-          }
-        ] });
+        const user = await User.findOne({ where: { username: params.username }, include: [{model: Post, include: [Likes]}, 'followers', 'following'] });
         return user;
+      },
+      homepage: async (parent, { user_id }) => {
+        const users = await User.findOne(
+          {
+            include: 
+            [
+              {model: Post, include: [Likes]}, 
+              "followers", 
+              {model: User, as: "following", include: {model: Post, include: [User, Likes]}}
+            ]
+          },
+          {where: {id: user_id}}
+        )
+        console.log(users)
+        return users;
       },
       getLikes: async (parent, { post_id }) => {
         const likes = await Likes.findAndCountAll({ where: { post_id: post_id }})
@@ -34,6 +50,9 @@ const resolvers = {
           include: [
           {
             model: User
+          },
+          {
+            model: Likes
           }
         ]
         }
@@ -69,18 +88,39 @@ const resolvers = {
         const token = signToken(user)
         return {token, user}
       },
-      createPost: async (parent, {post_text}, context) => {
-        return await Post.create({user_id: context.user.id, post_text})
+      createPost: async (parent, {post_text, user_id}) => {
+        return await Post.create({user_id, post_text})
       },
-      addLike: async (parent, {post_id}, context) => {
-        return await Likes.create({user_liked_by: context.user.id, post_id})
+      toggleLike: async (parent, {post_id, user_liked_by}) => {
+        const checkLikes = await Likes.findAll({where: {post_id: post_id}})
+        console.log(`CheckLikes Length: ${checkLikes.length}`)
+        for (i = 0; i < checkLikes.length; i++) {
+          if (checkLikes[i].user_liked_by == user_liked_by) {
+            console.log(`deleting likes where id: ${checkLikes[i].id}`)
+            await Likes.destroy({where: {id: checkLikes[i].id}})
+            return -1;
+          }
+          console.log(`CheckLikes Look Post Id ${checkLikes[i].id} CheckLikes User ${checkLikes[i].user_liked_by}`)
+        }
+        console.log("creating like")
+        await Likes.create({user_liked_by, post_id})
+        return 1;
       },
-      addFollow: async (parent, {followed}, context) => {
-        return await Follows.create({followed_id: followed, follower_id: context.user.id})
+      toggleFollow: async (parent, {followed, user_id}) => {
+        const checkFollows = await Follows.findAll({where: {follower_id: user_id}})
+        for (i = 0; i < checkFollows.length; i++) {
+          if (checkFollows[i].followed_id == followed) {
+            console.log(`deleting follow where id: ${checkFollows[i].id}`)
+            await Follows.destroy({where: {id: checkFollows[i].id}})
+            return false;
+          }
+          console.log(`checkFollows Look Post Id ${checkFollows[i].id} checkFollows User ${checkFollows[i].user_liked_by}`)
+        }
+        await Follows.create({followed_id: followed, follower_id: user_id})
+        return true;
       },
       login: async (parent, { username, password }) => {
         const user = await User.findOne({where: { username: username }});
-        console.log(user)
         if (!user) {
           throw new AuthenticationError('Incorrect credentials');
         }
