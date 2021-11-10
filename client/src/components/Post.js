478d7d4ba1d8e5b7435dda5a821ea0ccd2291258
Dominick Simone <domsimone123@gmail.com>
2021-11-10 13:48:31 -0500

    adding likes updating on change

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,26 +1,42 @@
 import {Link} from "react-router-dom"
-import { useQuery } from '@apollo/client';
+import {useState} from "react"
+import { useMutation, useQuery } from '@apollo/client';
 import { GET_LIKES } from '../utils/queries';
+import { TOGGLE_LIKE } from "../utils/mutations"
+import getMonthDay from "../utils/dateFormatter";
+import Auth from '../utils/auth';
 
-const Post = ({username, firstName, postText, postId}) => {
+const Post = ({username, firstName, postText, postId, createdAt, likes}) => {
 
-    const { loading, data } = useQuery(GET_LIKES, {
-        variables: { post_id: parseInt(postId) },
-      });
-      const likes = data?.getLikes || 0;
-      if(loading) {
-          return <h1></h1>;
+
+    const [likeCount, setLikeCount] = useState(likes)
+    const [toggleLike, { error, data }] = useMutation(TOGGLE_LIKE, {
+        onCompleted: (data) => setLikeCount(likeCount + data.toggleLike)
+    })
+    
+    const handleClick = async () => {
+        try {
+            const {data} = await toggleLike({variables: {post_id: parseInt(postId), user_liked_by: parseInt(Auth.getProfile().data.id)}}
+)           
+        } catch (err) {
+            console.log(err)
         }
+    }
+    const datePosted = getMonthDay(parseInt(createdAt))
     return (
         <div className="singlePostOuterDiv">
             <div className="singlePostHeaderDiv">
-                <h4 className="marginOne">{firstName} @<Link to={`/${username}`}>{username}</Link></h4>
+                <h4 className="marginOne">{firstName} @<Link to={`/${username}`}>{username}</Link></h4><p>{datePosted}</p>
             </div>
             <div className="singlePostTextDiv">
                 <p>{postText}</p>
             </div>
             <div className="singlePostFooterDiv">
-                <h4 className="marginOne" datapostid={postId}>Likes {likes}</h4>
+                {Auth.loggedIn() ? (
+                    <>
+                    <button className="marginOne postButton" onClick={() => handleClick()} datapostid={postId}>Like</button><h4>{likeCount}</h4></>) :
+                    <h4>Likes: {likeCount}</h4>
+                }
             </div>
         </div>
     )
