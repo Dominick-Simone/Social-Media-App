fae9fdc0a27074ef331ab246e162124231e673e5
Dominick Simone <domsimone123@gmail.com>
2021-11-16 17:13:33 -0500

    adding followers updating on change

--- react-ui/src/pages/Dashboard.js
+++ react-ui/src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Post from "../components/Post"
 import { useQuery, useMutation } from '@apollo/client';
 import { useParams } from 'react-router-dom';
@@ -10,7 +10,20 @@ import Auth from '../utils/auth';
 const Dashboard = () => {
     const { loading, data } = useQuery(QUERY_DASHBOARD);
     const user = data?.dashboard || [];
-    const [toggleFollow, { error, likeData }] = useMutation(TOGGLE_FOLLOW)
+    const [following, setFollowing] = useState(0)
+    const [followers, setFollowers] = useState(0)
+    const [toggleFollow, { error, followData }] = useMutation(TOGGLE_FOLLOW, {
+        onCompleted: (data) => {
+            setFollowers(followers + data.toggleFollow) 
+            setFollowing(following + data.toggleFollow) 
+        }
+    })
+    useEffect(() => {
+        if (data) {
+            setFollowers(user.followers.length)
+            setFollowing(user.following.length)
+        }
+    },[data])
     if (loading) {
         return <h1></h1>;
     }
@@ -20,7 +33,7 @@ const Dashboard = () => {
             <div className="profileOuterDiv">
                 <div className="profileHeaderDiv">
                     <h3 className="marginOne">{user.first_name}  @{user.username}</h3>
-                    <h4 className="marginOne">Followers: {user.followers.length} Following: {user.following.length}</h4>
+                    <h4 className="marginOne">Followers: {followers} Following: {following}</h4>
                     <button className="followButton" onClick={() => toggleFollow({variables: {followed: parseInt(user.id), user_id: parseInt(Auth.getProfile().data.id)}})}>Follow</button>
                 </div>
                 <div className="profileTextDiv">
