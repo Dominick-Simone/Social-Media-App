4d44e9e71ead8eca13daf82cc4ce74719933a6cb
Dominick Simone <domsimone123@gmail.com>
2021-11-16 18:31:07 -0500

    adding comment model relationships

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,7 @@ const User = require("./User")
 const Post = require("./Post")
 const Likes = require("./Likes")
 const Follows = require("./Follows")
+const Comments = require("./Comments")
 
 User.hasMany(Post, {
     foreignKey: "user_id"
@@ -27,9 +28,24 @@ Likes.belongsTo(Post, {
     foreignKey: "post_id",
 })
 
+Post.hasMany(Comments, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE"
+})
+Comments.belongsTo(Post, {
+    foreignKey: "post_id"
+})
+User.hasMany(Comments, {
+    foreignKey: "author_id",
+    onDelete: "CASCADE"
+})
+Comments.belongsTo(User, {
+    foreignKey: "author_id"
+})
+
 User.belongsToMany(User, {through: "follows", foreignKey: "follower_id", otherKey: "followed_id", as:"following"})
 User.belongsToMany(User, {through: "follows", foreignKey: "followed_id", otherKey: "follower_id", as:"followers"})
 
 
 
-module.exports = {Post, User, Likes, Follows}
+module.exports = {Post, User, Likes, Follows, Comments}
