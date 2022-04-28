786d5825da78736d97e6736e2daf1eff702eccf7
Dominick Simone <domsimone123@gmail.com>
2022-04-27 21:32:53 -0400

    adding functionality for follow button to change if following

--- react-ui/src/pages/Profile.js
+++ react-ui/src/pages/Profile.js
@@ -4,11 +4,12 @@ import { useQuery, useMutation } from '@apollo/client';
 import { useParams } from 'react-router-dom';
 import { QUERY_USER, QUERY_USER_BY_USERNAME } from '../utils/queries';
 import getMonthDay from '../utils/dateFormatter';
-import { TOGGLE_FOLLOW } from '../utils/mutations';
+import { TOGGLE_FOLLOW, CHECK_FOLLOW } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const Profile = () => {
     let { username } = useParams();
+    const [followed, setFollowed] = useState(false)
     const [following, setFollowing] = useState(0)
     const [followers, setFollowers] = useState(0)
     const { loading, data } = useQuery(QUERY_USER_BY_USERNAME, {
@@ -16,12 +17,21 @@ const Profile = () => {
     });
     const user = data?.user || [];
     const [toggleFollow, { error, followData }] = useMutation(TOGGLE_FOLLOW, {
-        onCompleted: (data) => setFollowers(followers + data.toggleFollow)
+        onCompleted: (data) => {
+            setFollowers(followers + data.toggleFollow)
+            setFollowed(!followed)
+        }
+    })
+    const [checkFollow, { checkFollowError, checkFollowData }] = useMutation(CHECK_FOLLOW, {
+        onCompleted: (data) => data.checkFollow == 1 ? setFollowed(true) : setFollowed(false)
     })
     useEffect(() => {
         if (data) {
             setFollowers(user.followers.length)
             setFollowing(user.following.length)
+            checkFollow( {
+                variables: { user_id: user.id },
+            })
         }
     },[data])
     if (loading) {
@@ -43,7 +53,7 @@ const Profile = () => {
                     <h3 className="marginOne">{user.first_name}  @{user.username}</h3>
                     {Auth.loggedIn() ? (
                         <>
-                        <button className="followButton" onClick={() => toggleFollow({variables: {followed: parseInt(user.id), user_id: parseInt(Auth.getProfile().data.id)}})}>Follow</button>
+                        <button className="followButton" onClick={() => toggleFollow({variables: {followed: parseInt(user.id)}})}>{followed ? "Following" : "Follow"}</button>
                     </>
                     ) : (
                         <p></p>
